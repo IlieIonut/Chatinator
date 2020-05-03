324aee4c571f3458cd55a6caf293b6185106b472
Dinescu Alexandru-Catalin <alexdinescu666@yahoo.com>
2020-05-03 03:51:40 +0300

    Fixed a conflict

--- app/src/main/java/com/example/chatinator/MainActivity.kt
+++ app/src/main/java/com/example/chatinator/MainActivity.kt
@@ -2,23 +2,21 @@ package com.example.chatinator
 
 import android.content.ContentValues
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login_layout.*
+import kotlinx.android.synthetic.main.register_layout.*
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import kotlinx.android.synthetic.main.test.*
-import kotlinx.android.synthetic.main.register_layout.*
-import android.view.View
-import kotlin.math.log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 private  const val TAG = "MainActivity"
 
@@ -28,57 +26,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
-        //SA NU STERGI CHESTIILE ASTEA
-        window.setBackgroundDrawableResource(R.drawable.background)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-    //DE AICI IN JOS POTI SA FACI CE VREI
-
-
-
-}
-
-
-
-/*        val sortColumn = UsersContract.Columns.USERS_EMAIL
-
-//        val cursor = contentResolver.query(UsersContract.buildUriFromId(1)
+        setContentView(R.layout.login_layout)
 
         LoginButton.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0 : View?)
             {
-                val name = NameText.text.toString()
-                val password = PasswordText.text.toString()
+                val name = usernameLogin.text.toString()
+                val password = passwordLogin.text.toString()
 
                 val cursor = contentResolver.query(UsersContract.CONTENT_URI,
                     null,
@@ -95,6 +50,24 @@ class MainActivity : AppCompatActivity() {
                                 if(name == nameDb && password == passDb)
                                 {
                                     setContentView(R.layout.activity_main)
+                                    window.setBackgroundDrawableResource(R.drawable.background)
+                                    val toolbar: Toolbar = findViewById(R.id.toolbar)
+                                    setSupportActionBar(toolbar)
+
+                                    val fab: FloatingActionButton = findViewById(R.id.fab)
+                                    fab.setOnClickListener { view ->
+                                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                            .setAction("Action", null).show()
+                                    }
+                                    val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+                                    val navView: NavigationView = findViewById(R.id.nav_view)
+                                    val navController = findNavController(R.id.nav_host_fragment)
+                                    // Passing each menu ID as a set of Ids because each
+                                    // menu should be considered as top level destinations.
+                                    appBarConfiguration = AppBarConfiguration(setOf(
+                                        R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                                    setupActionBarWithNavController(navController, appBarConfiguration)
+                                    navView.setupWithNavController(navController)
                                 }
                             }
                         }
@@ -111,10 +84,10 @@ class MainActivity : AppCompatActivity() {
 
                 ConfirmRegisterButton.setOnClickListener(object : View.OnClickListener{
                     override fun onClick(v: View?) {
-                        val name = "Test"
-                        val password = userPassword.text.toString()
-                        val confirmPassword = userCPassword.text.toString()
-                        val email = userEmail.text.toString()
+                        val name = usernameRegister.text.toString()
+                        val password = passwordRegister.text.toString()
+                        val confirmPassword = passwordConfirm.text.toString()
+                        val email = emailRegister.text.toString()
 
                         if(password == confirmPassword) {
 
@@ -125,7 +98,7 @@ class MainActivity : AppCompatActivity() {
                                 null,
                                 null,
                                 null,
-                                sortColumn
+                                null
                             )
 
                             cursor.use {
@@ -150,13 +123,45 @@ class MainActivity : AppCompatActivity() {
 
                                 contentResolver.insert(UsersContract.CONTENT_URI, values)
                                 setContentView(R.layout.activity_main)
+                                window.setBackgroundDrawableResource(R.drawable.background)
+                                val toolbar: Toolbar = findViewById(R.id.toolbar)
+                                setSupportActionBar(toolbar)
+
+                                val fab: FloatingActionButton = findViewById(R.id.fab)
+                                fab.setOnClickListener { view ->
+                                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show()
+                                }
+                                val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+                                val navView: NavigationView = findViewById(R.id.nav_view)
+                                val navController = findNavController(R.id.nav_host_fragment)
+                                // Passing each menu ID as a set of Ids because each
+                                // menu should be considered as top level destinations.
+                                appBarConfiguration = AppBarConfiguration(setOf(
+                                    R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                                setupActionBarWithNavController(navController, appBarConfiguration)
+                                navView.setupWithNavController(navController)
                             }
                         }
                     }
                 })
 
             }
-        })*/
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
+
 //        val toolbar: Toolbar = findViewById(R.id.toolbar)
 //        setSupportActionBar(toolbar)
 //
