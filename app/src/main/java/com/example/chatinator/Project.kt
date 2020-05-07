de7532327b0113bbc7aa43c879d9d0172c807102
Dinescu Alexandru-Catalin <alexdinescu666@yahoo.com>
2020-05-08 01:42:13 +0300

    Connected the projects and companies to the data base

--- app/src/main/java/com/example/chatinator/Project.kt
+++ app/src/main/java/com/example/chatinator/Project.kt
@@ -1,9 +1,12 @@
 package com.example.chatinator
 
-class Project (private val id :  String, name : String, workers : Int, tasks : Int,companies : Int) : DatabaseElement(name){
+class Project (id :  String, name : String, workers : Int, tasks : Int,companies : Int) : DatabaseElement(name){
      val workers = workers
      val tasks = tasks
      val companies = companies
+     val id = id
+
+    constructor() : this("","",0,0,0) {}
 
      override fun toString(): String {
           return """
