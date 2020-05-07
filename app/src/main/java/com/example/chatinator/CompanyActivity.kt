4c2fb039ab7951e6f6877af1084d914c1c7843f4
Dinescu Alexandru-Catalin <alexdinescu666@yahoo.com>
2020-05-08 00:03:31 +0300

    Implemented the insert function directly into firebase database

--- app/src/main/java/com/example/chatinator/CompanyActivity.kt
+++ app/src/main/java/com/example/chatinator/CompanyActivity.kt
@@ -2,7 +2,6 @@ package com.example.chatinator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.companies_layout.*
 
 class CompanyActivity : AppCompatActivity() {
 
@@ -11,39 +10,10 @@ class CompanyActivity : AppCompatActivity() {
 
         setContentView(R.layout.companies_layout)
 
-        val cursor = contentResolver.query(
-            CompaniesContract.CONTENT_URI,
-            null,
-            null,
-            null,
-            null
-        )
 
-        var companies = ArrayList<Company>()
-
-//        val values = ContentValues().apply {
-//            put(CompaniesContract.Columns.COMPANIES_NAME, "SOFTBIN")
-//            put(CompaniesContract.Columns.COMPANIES_EMPLOYEES, 15)
-//            put(CompaniesContract.Columns.COMPANIES_PROJECT, 5)
-//        }
 //
-//        contentResolver.insert(CompaniesContract.CONTENT_URI, values)
-
-        cursor.use {
-            if (it != null) {
-                while (it.moveToNext()) {
-                    with(cursor) {
-                        val newCompany = Company()
-                        newCompany.name = this?.getString(1).toString()
-                        newCompany.employees = this?.getInt(2)!!
-                        newCompany.projects = this?.getInt(3)
-                        companies.add(newCompany)
-
-                    }
-                }
-            }
-            val companyAdapter = CustomAdapter(this, R.layout.companies_item, companies,2)
-            companiesListView.adapter = companyAdapter
+//            val companyAdapter = CustomAdapter(this, R.layout.companies_item, companies,2)
+//            companiesListView.adapter = companyAdapter
         }
     }
-}
+
